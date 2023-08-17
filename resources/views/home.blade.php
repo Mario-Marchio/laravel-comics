a3b1c30ecb88579bbe093bd5158a97e76c447a33
Mario-Marchio <mariomarchio1610@gmail.com>
2023-08-17 04:05:22 +0200

    header done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    ciao
+    <header>
+        <div class="h-40 bgc-black"></div>
+        <div class="container header-nav">
+            <figure>
+                <img class="p-10" src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+            </figure>
+            <div class="r-header-nav">
+                <ul class="d-flex">
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">TV</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Games</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">News</a>
+                    </li>
+                    <li>
+                        <a class="color-black px-55 py-5 " href="#">Shop</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <main></main>
+        <footer></footer>
 </body>
 
 </html>
