f7dd3cbe57a37b49c64499675b823a08974cca4f
Mario-Marchio <mariomarchio1610@gmail.com>
2023-08-21 05:28:11 +0200

    added main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,81 @@
                 @endforeach
             </div>
         </div>
-        <main></main>
+        <main>
+            <div class="bgc-l-blue">
+                <div class="container nav-main">
+                    <ul class="d-flex">
+                        <li class="nav-main-li">
+                            <img class="nav-main-img" src="{{ asset('img/buy-comics-digital-comics.png') }}"
+                                alt="">
+                            <a class="nav-main-a" href="#">DIGITAL COMICS</a>
+                        </li>
+                        <li class="nav-main-li">
+                            <img class="nav-main-img" src="{{ asset('img/buy-comics-merchandise.png') }}"
+                                alt="">
+                            <a class="nav-main-a" href="#">DC MERCHANDISE</a>
+                        </li>
+                        <li class="nav-main-li">
+                            <img class="nav-main-img" src="{{ asset('img/buy-comics-subscriptions.png') }}"
+                                alt="">
+                            <a class="nav-main-a" href="#">SUBSCRIPTION</a>
+                        </li>
+                        <li class="nav-main-li">
+                            <img class="nav-main-img" src="{{ asset('img/buy-comics-shop-locator.png') }}"
+                                alt="">
+                            <a class="nav-main-a" href="#">COMIC SHOP LOCATOR</a>
+                        </li>
+                        <li class="nav-main-li">
+                            <img class="nav-main-img" src="{{ asset('img/buy-dc-power-visa.png') }}" alt="">
+                            <a class="nav-main-a" href="#">DC POWER VISA</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="dc-bg">
+                <div class="container d-flex">
+                    <div class="main-container">
+                        <div class="main-list-name">DC COMICS</div>
+                        <ul>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                        </ul>
+                        <div class="main-list-name">SHOP</div>
+                        <ul>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                        </ul>
+                    </div>
+                    <div class="main-container">
+                        <div class="main-list-name">DC</div>
+                        <ul>
+
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                        </ul>
+                    </div>
+                    <div class="main-container">
+                        <div class="main-list-name">SITES</div>
+                        <ul>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                            <li><a class="main-a" href="#">lorem</a></li>
+                        </ul>
+                    </div>
+                    <img class="img-main" src="{{ asset('img/dc-logo-bg.png') }}" alt="">
+
+                </div>
+            </div>
+        </main>
         <footer></footer>
 </body>
 
